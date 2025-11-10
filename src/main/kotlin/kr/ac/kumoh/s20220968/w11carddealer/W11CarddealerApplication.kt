88db41618eaff80ac5b7664fb1c0437f2bd0f8dc
sunghun <ishun0421@naver.com>
2025-11-10 15:25:10 +0900

    🎉프로젝트 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20220968/w11carddealer/W11CarddealerApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20220968.w11carddealer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W11CarddealerApplication
+
+fun main(args: Array<String>) {
+    runApplication<W11CarddealerApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20220968/w11carddealer/W11CarddealerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220968.w11carddealer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W11CarddealerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
